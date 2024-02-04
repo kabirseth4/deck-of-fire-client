882b218b1b0a091e5a5073bd611a3eb8eac89890
kabirseth4 <kabirseth4@gmail.com>
2024-02-04 20:50:01 +0000

    Style textarea

--- src/components/Textarea/Textarea.jsx
+++ src/components/Textarea/Textarea.jsx
@@ -14,19 +14,21 @@ export const Textarea = ({
       <label className="textarea__label">
         {label}
         <textarea
-          className="textarea__input"
+          className={`textarea__input${error ? " textarea__input--error" : ""}`}
           name={name}
           placeholder={placeholder || label}
           value={value}
           onChange={onChange}
         />
       </label>
-      {error && <p className="textarea__error">{error}</p>}
-      {count && (
-        <p className="textarea__count">
-          {value.length}/{count}
-        </p>
-      )}
+      <div className="textarea__validation">
+        {count && (
+          <p className="textarea__count">
+            {value.length}/{count}
+          </p>
+        )}
+        {error && <p className="textarea__error">{error}</p>}
+      </div>
     </div>
   );
 };
