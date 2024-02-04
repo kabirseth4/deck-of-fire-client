9dd5f95e028d497007b43445f6b7dd6200eec66f
kabirseth4 <kabirseth4@gmail.com>
2024-02-04 15:36:00 +0000

    Add rules page buttons

--- src/pages/RulesPage/RulesPage.jsx
+++ src/pages/RulesPage/RulesPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { RuleList } from "../../components/RuleList/RuleList";
 import { useGetRules } from "../../hooks/useGetRules";
+import { LinkButton } from "../../components/LinkButton/LinkButton";
 import { Button } from "../../components/Button/Button";
 import { CreateRuleModal } from "../../components/CreateRuleModal/CreateRuleModal";
 import "./RulesPage.scss";
@@ -15,14 +16,22 @@ export const RulesPage = () => {
   return (
     <main className={`rules-page${showModal ? " rules-page--no-scroll" : ""}`}>
       <h1 className="rules-page__titles">Rules</h1>
-      <Button
-        label="Create rule"
-        onClick={() => {
-          setShowModal(true);
-        }}
-      />
       <RuleList rules={rules} />
       {showModal && <CreateRuleModal setShowModal={setShowModal} />}
+      <div className="rules-page__page-buttons">
+        <LinkButton
+          to="/decks"
+          className="button--secondary rules-page__page-button"
+          label="Decks"
+        />
+        <Button
+          label="Create rule"
+          className="rules-page__page-button"
+          onClick={() => {
+            setShowModal(true);
+          }}
+        />
+      </div>
     </main>
   );
 };
