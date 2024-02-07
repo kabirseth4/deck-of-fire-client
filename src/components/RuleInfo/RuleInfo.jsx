90f558a8a9b4ab28a2417add1dd714878706063b
kabirseth4 <kabirseth4@gmail.com>
2024-02-07 15:34:01 +0000

    Style rules page

--- src/components/RuleInfo/RuleInfo.jsx
+++ src/components/RuleInfo/RuleInfo.jsx
@@ -3,24 +3,26 @@ import "./RuleInfo.scss";
 export const RuleInfo = ({ rule, deckDetails }) => {
   return (
     <article className="rule-info">
-      <h3 className="rule-info__title">{rule.name}</h3>
-      <p className="rule-info__description">{rule.description}</p>
-      {deckDetails && (deckDetails.isCustom || deckDetails.isScored) && (
-        <div className="rule-info__info">
-          {deckDetails.isScored && (
-            <div className="rule-info__info-container">
-              <p className="rule-info__info-item">Penalty: {rule.penalty}</p>
-            </div>
-          )}
-          {deckDetails.isCustom && (
-            <div className="rule-info__info-container">
-              <p className="rule-info__info-item">
-                Occurences: {rule.occurences}
-              </p>
-            </div>
-          )}
-        </div>
-      )}
+      <div className="rule-info__container">
+        <h3 className="rule-info__title">{rule.name}</h3>
+        <p className="rule-info__description">{rule.description}</p>
+        {deckDetails && (deckDetails.isCustom || deckDetails.isScored) && (
+          <div className="rule-info__info">
+            {deckDetails.isScored && (
+              <div className="rule-info__info-container">
+                <p className="rule-info__info-item">Penalty: {rule.penalty}</p>
+              </div>
+            )}
+            {deckDetails.isCustom && (
+              <div className="rule-info__info-container">
+                <p className="rule-info__info-item">
+                  Occurences: {rule.occurences}
+                </p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
     </article>
   );
 };
