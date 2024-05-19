7c2c5b38a7bbc61d9b4dfbff68da9fae4a7985e7
kabirseth4 <kabirseth4@gmail.com>
2024-05-19 18:44:35 +0100

    Refactor: convert providers folder to ts

---
+++ src/providers/authProvider.tsx
@@ -0,0 +1,47 @@
+import {
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+interface User {
+  id: string;
+  token: string;
+}
+
+interface AuthContextType {
+  user: User;
+  setUser: Dispatch<SetStateAction<User>>;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const useAuthContext = () => {
+  return useContext(AuthContext) as AuthContextType;
+};
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User>(
+    JSON.parse(localStorage.getItem("user")!)
+  );
+
+  useEffect(() => {
+    user
+      ? localStorage.setItem("user", JSON.stringify(user))
+      : localStorage.removeItem("user");
+  }, [user]);
+
+  const contextValue: AuthContextType = useMemo(
+    () => ({ user, setUser }),
+    [user]
+  );
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
